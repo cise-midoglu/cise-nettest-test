0d0998f7cf857995897066a61f5e35ae521a928e
Cise Midoglu <cise@simula.no>
2018-02-01 10:25:51 +0100

    Container tag updated to "monroe-nettest-client".

--- nettest-client/push.sh
+++ nettest-client/push.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CONTAINER=${DIR##*/}
 
-CONTAINERTAG=cmidoglu/nettest-test
+CONTAINERTAG=cmidoglu/monroe-nettest-client
 
 docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
